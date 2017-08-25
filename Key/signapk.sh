b88d64c0a8080957799bb02050bc4a2453dda383
Bill Youngdi <yang@Billde-MacBook-Pro.local>
2017-08-25 12:37:27 +0800

    upgrade android

---
+++ Key/signapk.sh
@@ -0,0 +1,3 @@
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore directions.keystore android-release-unsigned.apk directions
+~/Library/Android/sdk/build-tools/26.0.1/zipalign -v 4 android-release-unsigned.apk directions.apk
