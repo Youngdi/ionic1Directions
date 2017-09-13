1c64e8f44b858144a8de91e4b48c176ffae80922
Bill Youngdi <yang@Billde-MacBook-Pro.local>
2017-09-13 21:44:16 +0800

    refactor android

--- Key/signapk.sh
+++ Key/signapk.sh
@@ -1,3 +1,7 @@
-cp /Users/yang/Desktop/directions/platforms/android/build/outputs/apk/android-release-unsigned.apk ./
+rm directions.apk
+rm android-release-unsigned.apk
+cp /Users/yang/Documents/directions/platforms/android/build/outputs/apk/android-release-unsigned.apk ./
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore directions.keystore android-release-unsigned.apk directions
 ~/Library/Android/sdk/build-tools/26.0.1/zipalign -v 4 android-release-unsigned.apk directions.apk
+
+
